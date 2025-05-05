84449a68b91bda6d50042742d8af814dd073c6ea
ksbuin <ksubin1017@gmail.com>
2025-05-05 12:05:26 +0900

    baekjoon 12789

--- src/Main.java
+++ src/Main.java
@@ -5,51 +5,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        while (true) {
-            String str = br.readLine();
-            Stack<Character> stack = new Stack<>();
-            if(str.equals(".")) break;
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
+        int n = Integer.parseInt(br.readLine());
 
-                if(c == '(' || c == '[') {
-                    stack.push(c);
-                }
+        Queue<Integer> queue = new LinkedList<>(); 
+        Stack<Integer> stack2 = new Stack<>();
 
-                if(c == ')') {
-                    if(stack.isEmpty()) {
-                        stack.push(c);
-                        break;
-                    }
+        int cnt = 1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-                    if(stack.peek() == '(') {
-                        stack.pop();
-                    } else {
-                        break;
-                    }
-                } else if(c == ']') {
-                    if(stack.isEmpty()) {
-                        stack.push(c);
-                        break;
-                    }
-
-                    if(stack.peek() == '[') {
-                        stack.pop();
-                    } else {
-                        break;
-                    }
-                }
-            }
-
-            if(stack.isEmpty()) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
+        for (int i = 0; i < n; i++) {
+            queue.offer(Integer.parseInt(st.nextToken()));
         }
 
+        while (true) {
+            if (queue.isEmpty() && stack2.isEmpty()) {
+                System.out.println("Nice");
+                break;
+            }
 
+            if (!queue.isEmpty() && queue.peek() == cnt) {
+                queue.poll();
+                cnt++;
+            } else if (!stack2.isEmpty() && stack2.peek() == cnt) {
+                stack2.pop();
+                cnt++;
+            } else if (!queue.isEmpty()) {
+                stack2.push(queue.poll());
+            } else {
+                System.out.println("Sad");
+                break;
+            }
+        }
     }
 }
