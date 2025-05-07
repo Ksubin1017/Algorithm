72032ea9f8b78a5fe899f0cc763a6a5162dc0bbf
ksbuin <ksubin1017@gmail.com>
2025-05-07 11:04:53 +0900

    baekjoon 11866

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         Queue<Integer> queue = new LinkedList<>();
 
         for(int i = 1; i <= n; i++) {
-            queue.offer(i);
+            queue.add(i);
         }
 
         while(queue.size() > 1) {
-            queue.poll();
+            for(int i = 0; i < k-1; i++) {
+                int temp = queue.poll();
+                queue.add(temp);
+            }
 
-            int a = queue.poll();
-            queue.offer(a);
+            int num = queue.poll();
+            sb.append(num).append(", ");
         }
-        System.out.println(queue.poll());
+
+        int num = queue.poll();
+        sb.append(num).append(">");
+
+        System.out.println(sb.toString());
     }
 }
