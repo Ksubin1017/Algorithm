b651b4de414eb54a88653b3f69269bf91464adeb
ksbuin <ksubin1017@gmail.com>
2025-05-07 10:56:04 +0900

    baekjoon 2164

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,18 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        LinkedList<Integer> queue = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
 
-        for(int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            String command = st.nextToken();
-
-            if(command.equals("push")) {
-                int x = Integer.parseInt(st.nextToken());
-                queue.offer(x);
-            } else if(command.equals("pop")) {
-                if(queue.isEmpty()) {
-                    System.out.println("-1");
-                } else {
-                    int x = queue.poll();
-                    System.out.println(x);
-                }
-            } else if(command.equals("size")) {
-                System.out.println(queue.size());
-            } else if(command.equals("empty")) {
-                if(!queue.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(1);
-                }
-            } else if(command.equals("front")) {
-                if(!queue.isEmpty()) {
-                    System.out.println(queue.peek());
-                } else {
-                    System.out.println(-1);
-                }
-            } else if(command.equals("back")) {
-                if(!queue.isEmpty()) {
-                    System.out.println(queue.peekLast());
-                } else {
-                    System.out.println(-1);
-                }
-            }
+        for(int i = 1; i <= n; i++) {
+            queue.offer(i);
         }
+
+        while(queue.size() > 1) {
+            queue.poll();
+
+            int a = queue.poll();
+            queue.offer(a);
+        }
+        System.out.println(queue.poll());
     }
 }
