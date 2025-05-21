12ef66a91e30f1697e8fcba5328f2eb7f3936ded
ksbuin <ksubin1017@gmail.com>
2025-05-21 12:59:08 +0900

    baekjoon 15649

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,39 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
+    static int N, M;
+    static int[] arr = new int[M + 1];
+    static boolean[] visited = new boolean[N + 1];
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt(); // 숫자 범위
+        M = sc.nextInt(); // 몇 개 뽑을지
 
-        System.out.println(fibo(n));
+        arr = new int[M];        // 선택된 숫자 저장
+        visited = new boolean[N+1]; // 숫자 사용 여부
 
+        back(0); // 깊이 0부터 시작
     }
 
-    static long fibo(int x) {
-        if(x == 0) return 0;
-        if(x == 1) return 1;
+    static void back(int position) {
+        if(position == M) {
+            for(int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
 
-        return fibo(x - 1) + fibo(x - 2);
+        for(int i = 1; i <= N; i++) {
+            if(!visited[i]) {
+                arr[position] = i;
+                visited[i] = true;
+                back(position + 1);
+                visited[i] = false;
+            }
+        }
     }
+
 }
