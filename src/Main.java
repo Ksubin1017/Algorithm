6af3c0d9961eaa479eff2364fd8a20440b766b78
ksbuin <ksubin1017@gmail.com>
2025-05-26 11:17:58 +0900

    baekjoon 15651

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,37 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[] arr = new int[M + 1];
-    static boolean[] visited = new boolean[N + 1];
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt(); // 숫자 범위
-        M = sc.nextInt(); // 몇 개 뽑을지
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];        // 선택된 숫자 저장
-        visited = new boolean[N+1]; // 숫자 사용 여부
+        arr = new int[M];
 
-        back(0, 1); // 깊이 0부터 시작
+        back(0);
+
+        System.out.print(sb.toString());
     }
 
-    static void back(int position, int start) {
-        if(position == M) {
-            for(int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
+    static void back(int position) {
+        if (position == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
-        for(int i = start; i <= N; i++) {
-            if(!visited[i]) {
-                arr[position] = i;
-//                visited[i] = true;
-                back(position + 1, i+ 1);
-//                visited[i] = false;
-            }
+        for (int i = 1; i <= N; i++) {
+            arr[position] = i;
+            back(position + 1);
         }
     }
-
 }
