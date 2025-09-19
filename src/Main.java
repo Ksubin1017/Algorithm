6f57237f3b906443c1c00c90ae17544c9d038258
ksbuin <ksubin1017@gmail.com>
2025-09-19 10:18:08 +0900

    DFS Memoization fibonacci

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] fibo;
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        fibo = new int[n + 1];
 
         T.DFS(n);
+        for(int i = 1; i <= n; i++) {
+            System.out.print(fibo[i] + " ");
+        }
     }
 
-    public void DFS(int n) {
-        if(n == 0) return;
-        else {
-            DFS(n / 2);
-            System.out.print(n % 2+ "");
-        }
+    public int DFS(int n) {
+        if(n == 1) return fibo[n] = 1;
+        else if(n == 2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n - 1) + DFS(n - 2);
     }
 }
