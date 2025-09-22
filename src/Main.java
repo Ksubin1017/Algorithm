be9eea5a021d8d4b61bae400ac9bef12649bf96d
ksbuin <ksubin1017@gmail.com>
2025-09-22 10:19:35 +0900

    baekjoon 2606

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] fibo;
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static boolean[][] graph;
+    public static boolean[] visited;
+    public static int answer, n, m;
 
-        int n = Integer.parseInt(br.readLine());
-        fibo = new int[n + 1];
+    public static void DFS(int idx) {
+        visited[idx] = true;
+        answer++;
 
-        T.DFS(n);
         for(int i = 1; i <= n; i++) {
-            System.out.print(fibo[i] + " ");
+            if(!visited[i] && graph[idx][i]) {
+                DFS(i);
+            }
         }
     }
 
-    public int DFS(int n) {
-        if(n == 1) return fibo[n] = 1;
-        else if(n == 2) return fibo[n] = 1;
-        else return fibo[n] = DFS(n - 1) + DFS(n - 2);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        graph = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
+        DFS(1);
+        bw.write(String.valueOf(answer - 1));
+        bw.close();
+        br.close();
     }
 }
