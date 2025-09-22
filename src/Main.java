da18d2ec47acf15aa08be5fee6434edd2b943f82
ksbuin <ksubin1017@gmail.com>
2025-09-22 11:12:47 +0900

    baekjoon 24479

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean[][] graph;
+    public static ArrayList<Integer>[] graph;
     public static boolean[] visited;
-    public static int answer, n, m;
+    public static int n, m, r, order;
+    public static int[] answer;
 
     public static void DFS(int idx) {
         visited[idx] = true;
-        answer++;
+        answer[idx] = order++;
 
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i] && graph[idx][i]) {
-                DFS(i);
-            }
+        for(int i = 0; i < graph[idx].size(); i++) {
+            int next = graph[idx].get(i);
+            if(!visited[next]) DFS(next);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        graph = new boolean[n + 1][n + 1];
-        visited = new boolean[n + 1];
 
-        for (int i = 1; i <= m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        answer = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        visited = new boolean[n + 1];
+        order = 1;
+        for(int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            graph[x][y] = true;
-            graph[y][x] = true;
+            graph[x].add(y);
+            graph[y].add(x);
         }
-        DFS(1);
-        bw.write(String.valueOf(answer - 1));
+
+        for(int i = 1; i <= n; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        DFS(r);
+
+        for(int i = 1; i <= n; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+
         bw.close();
         br.close();
     }
