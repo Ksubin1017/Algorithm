408ed1167427683e8c905f8c6ff7d322198422a4
ksbuin <ksubin1017@gmail.com>
2025-09-25 11:19:08 +0900

    baekjoon 11725

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m, start, end, answer = -1;
-    static boolean[][] graph;
+    static int n, m;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
+    static int[] answer;
 
-    public static void dfs(int idx, int count) {
+    public static void dfs(int idx) {
         visited[idx] = true;
-        if(idx == end) {
-            answer = count;
-            return;
-        }
 
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i] && graph[idx][i]) {
-                dfs(i, count + 1);
+        for(int i = 0; i < graph[idx].size(); i++) {
+            int next = graph[idx].get(i);
+            if(!visited[next]) {
+                answer[next] = idx;
+                dfs(next);
             }
         }
     }
@@ -24,27 +23,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int m = n - 1;
 
-        graph = new boolean[n + 1][n + 1];
+        graph = new ArrayList[n + 1];
         visited = new boolean[n + 1];
+        answer = new int[n + 1];
 
-        for(int i = 1; i <= m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = true;
-            graph[y][x] = true;
+        for(int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        dfs(start, 0);
+        for(int i = 1; i <= m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x].add(y);
+            graph[y].add(x);
+        }
 
-        bw.write(String.valueOf(answer));
+        dfs(1);
 
+        for(int i = 2; i <= n; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
         bw.close();
         br.close();
     }
