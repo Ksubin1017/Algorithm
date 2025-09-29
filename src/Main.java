99a1916326e1abd096efc993a418f174e75fd2a3
ksbuin <ksubin1017@gmail.com>
2025-09-29 10:39:28 +0900

    baekjoon 1012

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
-    static int[] answer;
+    static int max = 50 + 10;
+    static boolean[][] map;
+    static boolean[][] visited;
+    static int answer;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    public static void dfs(int idx) {
-        visited[idx] = true;
+    public static void dfs(int y, int x) {
+        visited[y][x] = true;
 
-        for(int i = 0; i < graph[idx].size(); i++) {
-            int next = graph[idx].get(i);
-            if(!visited[next]) {
-                answer[next] = idx;
-                dfs(next);
-            }
+        for(int i = 0; i < dx.length; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if(!visited[newY][newX] && map[newY][newX]) dfs(newY, newX);
         }
     }
 
@@ -23,31 +23,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = n - 1;
+        int t = Integer.parseInt(br.readLine());
 
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        answer = new int[n + 1];
-
-        for(int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for(int i = 1; i <= m; i++) {
+        while(t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x].add(y);
-            graph[y].add(x);
-        }
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
-        dfs(1);
+            map = new boolean[max][max];
+            visited = new boolean[max][max];
+            answer = 0;
 
-        for(int i = 2; i <= n; i++) {
-            bw.write(String.valueOf(answer[i]));
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                map[y + 1][x + 1] = true;
+            }
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= m; j++) {
+                    if (map[i][j] && !visited[i][j]) {
+                        dfs(i, j);
+                        answer++;
+                    }
+                }
+            }
+
+            bw.write(String.valueOf(answer));
             bw.newLine();
         }
+
         bw.close();
         br.close();
     }
