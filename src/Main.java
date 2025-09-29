2a60266feba1ac206ede1b3cbbed2ad2dc40487d
ksbuin <ksubin1017@gmail.com>
2025-09-29 11:03:38 +0900

    baekjoon 13565

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int max = 50 + 10;
+    static int n, m;
+    static int max = 1000 + 10;
     static boolean[][] map;
     static boolean[][] visited;
-    static int answer;
+    static String answer;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
     public static void dfs(int y, int x) {
         visited[y][x] = true;
+        if(y == m) {
+            answer = "YES";
+            return;
+        }
+        for(int k = 0; k < 4; k++) {
+            int ny = y + dy[k];
+            int nx = x + dx[k];
 
-        for(int i = 0; i < dx.length; i++) {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-            if(!visited[newY][newX] && map[newY][newX]) dfs(newY, newX);
+            if(!visited[ny][nx] && map[ny][nx]) {
+                dfs(ny, nx);
+            }
         }
     }
 
@@ -23,39 +30,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-        while(t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
+        map = new boolean[max][max];
+        visited = new boolean[max][max];
+        answer = "NO";
 
-            map = new boolean[max][max];
-            visited = new boolean[max][max];
-            answer = 0;
-
-            for (int i = 0; i < k; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                map[y + 1][x + 1] = true;
-            }
-
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= m; j++) {
-                    if (map[i][j] && !visited[i][j]) {
-                        dfs(i, j);
-                        answer++;
-                    }
+        for(int i = 1; i <= m; i++) {
+            String str = br.readLine();
+            for(int j = 1; j <= n; j++) {
+                if(str.charAt(j - 1) == '0') {
+                    map[i][j] = true;
                 }
             }
-
-            bw.write(String.valueOf(answer));
-            bw.newLine();
         }
 
+        for (int j = 1; j <= n; j++)
+            if (map[1][j]) dfs(1, j);
+
+        bw.write(answer);
         bw.close();
         br.close();
     }
