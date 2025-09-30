ecfb733bb7b2939a5aa464c32dd02ccce1478120
ksbuin <ksubin1017@gmail.com>
2025-09-30 11:25:19 +0900

    baekjoon 1388

--- src/Main.java
+++ src/Main.java
@@ -2,62 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean[][] graph;
+    static char[][] graph;
     static boolean[][] visited;
     static int answer;
-    static int max = 50 + 10;
-    static int n, m;
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int[] dx = {1, -1};
+    static int[] dy = {1, -1};
 
     public static void dfs(int y, int x) {
         visited[y][x] = true;
 
-        for(int k = 0; k < dx.length; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
+        if(graph[y][x] == '-' && graph[y][x + 1] == '-') dfs(y, x + 1);
+        else if(graph[y][x] == '|' && graph[y + 1][x] == '|') dfs(y + 1 , x);
 
-            if(!visited[ny][nx] && graph[ny][nx]) {
-                dfs(ny, nx);
-            }
-        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-            if(w == 0 && h == 0) break;
+        graph = new char[n + 10][m + 10];
+        visited = new boolean[n + 10][m + 10];
+        answer = 0;
 
-            graph = new boolean[max][max];
-            visited = new boolean[max][max];
-            answer = 0;
-
-            for(int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < w; j++) {
-                    int x = Integer.parseInt(st.nextToken());
-                    if(x == 1) graph[i + 1][j + 1] = true;
-                }
+        for(int i = 1; i <= n; i++) {
+            String s = br.readLine();
+            for(int j = 1; j <= m; j++) {
+                graph[i][j] = s.charAt(j - 1);
             }
-
-            for(int i = 1; i <= h; i++) {
-                for(int j = 1; j <= w; j++) {
-                    if(graph[i][j] && !visited[i][j]) {
-                        dfs(i, j);
-                        answer++;
-                    }
-                }
-            }
-
-            bw.write(answer + "\n");
         }
 
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(!visited[i][j]) {
+                    dfs(i, j);
+                    answer++;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(answer));
         bw.close();
         br.close();
     }
