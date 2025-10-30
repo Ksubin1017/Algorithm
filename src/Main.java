cc54df510d31f917ff6763e353f7a253adf1fb2c
ksbuin <ksubin1017@gmail.com>
2025-10-30 11:16:33 +0900

    baekjoon 2468

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean[][] graph;
+    static int[][] graph;
     static boolean[][] visited;
-    static int n, cnt, answer;
-    static int max = 25 + 10;
+    static int n, answer, cnt;
+    static int size = 100 + 10;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
 
-    static void dfs(int y, int x) {
+    static void dfs(int y, int x, int water) {
         visited[y][x] = true;
-        cnt++;
 
         for(int k = 0; k < 4; k++) {
             int nx = x + dx[k];
             int ny = y + dy[k];
-            if(!visited[ny][nx] && graph[ny][nx]) dfs(ny, nx);
+
+            if(!visited[ny][nx] && graph[ny][nx] > water) {
+                dfs(ny, nx, water);
+            }
         }
     }
 
@@ -24,36 +26,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int maxHeight = 0;
+        graph = new int[size][size];
         answer = 0;
-        graph = new boolean[max][max];
-        visited = new boolean[max][max];
-        ArrayList<Integer> list = new ArrayList<>();
 
         for(int i = 1; i <= n; i++) {
-            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 1; j <= n; j++) {
-                if(line.charAt(j - 1) == '1') graph[i][j] = true;
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                maxHeight = Math.max(maxHeight, graph[i][j]);
             }
         }
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(!visited[i][j] && graph[i][j]) {
-                    cnt = 0;
-                    dfs(i, j);
-                    answer++;
-                    list.add(cnt);
+        for(int water = 0; water <= maxHeight; water++) {
+            cnt = 0;
+            visited = new boolean[size][size];
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(!visited[i][j] && (graph[i][j] > water)) {
+                        dfs(i, j, water);
+                        cnt++;
+                    }
                 }
             }
+            answer = Math.max(answer, cnt);
         }
 
-        Collections.sort(list);
         bw.write(answer + "\n");
-
-        for(int i : list) {
-            bw.write(i + "\n");
-        }
         bw.close();
         br.close();
     }
