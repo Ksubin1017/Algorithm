120f14c2e54d08b35cb0041df87376a674e0fe9c
ksbuin <ksubin1017@gmail.com>
2025-11-04 11:25:41 +0900

    baekjoon 1987

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] graph;
-    static boolean[][] visited;
-    static int n, answer, cnt;
-    static int size = 100 + 10;
+    static char[][] graph;
+    static int answer;
+    static int max = 20 + 10;
+    static HashMap<Character, Integer> map;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
 
-    static void dfs(int y, int x, int water) {
-        visited[y][x] = true;
+    static void dfs(int y, int x, int cnt) {
+        map.put(graph[y][x], map.getOrDefault(graph[y][x], 0) + 1);
+        answer = Math.max(answer, cnt);
 
-        for(int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
+        for (int k = 0; k < 4; k++) {
             int ny = y + dy[k];
+            int nx = x + dx[k];
 
-            if(!visited[ny][nx] && graph[ny][nx] > water) {
-                dfs(ny, nx, water);
+            if(graph[ny][nx] != 0 && map.get(graph[ny][nx]) == null) {
+                dfs(ny, nx, cnt + 1);
+                map.remove(graph[ny][nx]);
             }
         }
     }
@@ -26,32 +28,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int maxHeight = 0;
-        graph = new int[size][size];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        graph = new char[max][max];
         answer = 0;
 
-        for(int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= n; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                maxHeight = Math.max(maxHeight, graph[i][j]);
+        map = new HashMap<>();
+
+        for(int i = 1; i <= r; i++) {
+            String str = br.readLine();
+            for(int j = 1; j <= c; j++) {
+                graph[i][j] = str.charAt(j - 1);
             }
         }
 
-        for(int water = 0; water <= maxHeight; water++) {
-            cnt = 0;
-            visited = new boolean[size][size];
-            for(int i = 1; i <= n; i++) {
-                for(int j = 1; j <= n; j++) {
-                    if(!visited[i][j] && (graph[i][j] > water)) {
-                        dfs(i, j, water);
-                        cnt++;
-                    }
-                }
-            }
-            answer = Math.max(answer, cnt);
-        }
+        map.put(graph[1][1], 1);
+        dfs(1, 1, 1);
+
 
         bw.write(answer + "\n");
         bw.close();
