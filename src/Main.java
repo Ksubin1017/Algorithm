22a9611d12db1b5572fd45111028e639050c2493
ksbuin <ksubin1017@gmail.com>
2025-11-10 10:30:41 +0900

    baekjoon 10815

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] graph;
-    static int[][] dp;
-    static int m, n;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    public int[] solution(HashMap<Integer,Integer> map, int[] target) {
 
-    static int dfs(int y, int x) {
-        if (y == m && x == n) return 1;
+        int[] answer = new int[target.length];
 
-        if (dp[y][x] != -1) return dp[y][x];
-
-        dp[y][x] = 0;
-
-        for (int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if (nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
-                if (graph[ny][nx] < graph[y][x]) {
-                    dp[y][x] += dfs(ny, nx);
-                }
+        for(int i = 0; i < target.length; i++) {
+            if(map.containsKey(target[i])) {
+                answer[i] = 1;
+            } else {
+                answer[i] = 0;
             }
         }
 
-        return dp[y][x];
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Main main = new Main();
+
+        int n = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-
-        graph = new int[m + 1][n + 1];
-        dp = new int[m + 1][n + 1];
-
-        for (int i = 1; i <= m; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = -1; // 아직 방문 안 한 상태
-            }
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            map.put(a, map.getOrDefault(a, 0) + 1);
         }
 
-        bw.write(dfs(1, 1) + "\n");
+        int m = Integer.parseInt(br.readLine());
+        int[] target = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            target[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i : main.solution(map, target)) {
+            bw.write(i + " ");
+        }
         bw.flush();
         bw.close();
         br.close();
