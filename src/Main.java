1a96e0ca4e705047055104e210aa1e69e2671933
ksbuin <ksubin1017@gmail.com>
2025-11-10 11:11:52 +0900

    baekjoon 7785

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(HashMap<String,Integer> map, String[] arr) {
-        int answer = 0;
-        for(String s : arr) {
-            if(map.containsKey(s)) {
-                answer++;
-            }
-        }
-
-        return answer;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Main main = new Main();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
+        int n = Integer.parseInt(br.readLine());
         HashMap<String, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < n; i++) {
-            String string = br.readLine();
-            map.put(string, map.getOrDefault(string, 0) + 1);
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String status = st.nextToken();
+
+            if(status.equals("leave")) {
+                map.put(name, map.getOrDefault(name, 0) - 1);
+            } else {
+                map.put(name, map.getOrDefault(name, 0) + 1);
+            }
         }
 
-        String[] arr = new String[m];
-
-        for(int i = 0; i < m; i++) {
-            arr[i] = br.readLine();
+        ArrayList<String> list = new ArrayList<>();
+        for(String name : map.keySet()) {
+            if(map.get(name) == 1) list.add(name);
         }
 
-        bw.write(main.solution(map, arr) + "");
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(String s : list) bw.write(s+"\n");
         bw.flush();
         bw.close();
         br.close();
