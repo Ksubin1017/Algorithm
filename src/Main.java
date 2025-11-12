0190bdbdd8d814ef6da732ec3121e4ef13e98aad
ksbuin <ksubin1017@gmail.com>
2025-11-12 11:04:44 +0900

    baekjoon 10816

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<Integer, String> map2 = new HashMap<>();
-
-        for(int i = 1; i <= n; i++) {
-            String s = br.readLine();
-
-            map1.put(s, i);
-            map2.put(i, s);
+        for(int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            map1.put(a, map1.getOrDefault(a, 0) + 1);
         }
 
-        for(int i = 1; i <= m; i++) {
-            String s = br.readLine();
-            if(49 <= s.charAt(0) &&  s.charAt(0) <= 57) {
-                int key = Integer.parseInt(s);
-                sb.append(map2.get(key)).append("\n");
-            } else {
-                sb.append(map1.get(s)).append("\n");
-            }
+        int m = Integer.parseInt(br.readLine());
+        int[] target = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++) {
+            target[i] = Integer.parseInt(st.nextToken());
         }
-        bw.write(sb.toString());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i : target) {
+            list.add(map1.getOrDefault(i, 0));
+        }
+
+        for (int i : list) {
+            bw.write(i + " ");
+        }
+
         bw.flush();
         bw.close();
         br.close();
