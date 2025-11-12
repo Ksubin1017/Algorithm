c74ee75a9d29e670ad3fc07a949874043c7a7c4f
ksbuin <ksubin1017@gmail.com>
2025-11-12 11:20:03 +0900

    baekjoon 1269

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-
-        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < n; i++) {
-            int a = Integer.parseInt(st.nextToken());
-            map1.put(a, map1.getOrDefault(a, 0) + 1);
-        }
-
-        int m = Integer.parseInt(br.readLine());
-        int[] target = new int[m];
+        HashMap<Integer, Integer> mapA = new HashMap<>();
+        HashMap<Integer, Integer> mapB = new HashMap<>();
 
         st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            mapA.put(a, mapA.getOrDefault(a, 0) + 1);
+        }
+
+        st = new StringTokenizer(br.readLine());
+
         for(int i = 0; i < m; i++) {
-            target[i] = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            mapB.put(b, mapB.getOrDefault(b, 0) + 1);
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
+        int cnt = 0;
 
-        for(int i : target) {
-            list.add(map1.getOrDefault(i, 0));
+        for(int i : mapB.keySet()) {
+            if(!mapA.containsKey(i)) cnt++;
         }
 
-        for (int i : list) {
-            bw.write(i + " ");
+        for(int i : mapA.keySet()) {
+            if(!mapB.containsKey(i)) cnt++;
         }
 
+        bw.write(String.valueOf(cnt));
         bw.flush();
         bw.close();
         br.close();
