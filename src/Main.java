2376c49399a585959d04c4a1600c37b158d74d88
ksbuin <ksubin1017@gmail.com>
2025-11-20 10:45:16 +0900

    baekjoon 27433

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,21 @@ import java.util.*;
 
 public class Main {
 
+    public static long factorial(int n) {
+        if(n == 1) return 1;
+        if(n == 0) return 1;
+
+        return n * factorial(n-1);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        bw.write(factorial(n)+"");
 
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i = 1; i <= n; i++) {
-            q.add(i);
-        }
-        sb.append("<");
-        while(q.size() > 1) {
-            for(int i = 0; i < k - 1; i++) {
-                q.add(q.poll());
-            }
-            sb.append(q.poll()).append(", ");
-        }
-
-        sb.append(q.poll()).append(">");
-        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
