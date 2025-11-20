ae73446ec6605a6ea354dbe6df5bba6b3c846f7d
ksbuin <ksubin1017@gmail.com>
2025-11-20 10:59:58 +0900

    baekjoon 25501

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int cnt;
+    public static int dfs(String s, int lt, int rt) {
+        cnt++;
+        if(lt >= rt) return 1;
+        else if(s.charAt(lt) != s.charAt(rt)) return 0;
+        else return dfs(s, lt + 1, rt - 1);
+    }
 
-    public static int dfs(int n) {
-
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 1;
-
-        return dfs(n-1) + dfs(n-2);
+    public static int isPalindrome(String s) {
+        return dfs(s, 0, s.length() - 1);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        bw.write(dfs(n)+"");
+        int t = Integer.parseInt(br.readLine());
 
+        while (t-- > 0) {
+            String s = br.readLine();
+            cnt = 0;
+            int result = isPalindrome(s);
+            sb.append(result).append(" ").append(cnt).append("\n");
+        }
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
