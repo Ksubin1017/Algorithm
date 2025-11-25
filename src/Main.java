afd2803d6b6619da5438150d85c128ad01819e90
ksbuin <ksubin1017@gmail.com>
2025-11-25 11:09:23 +0900

    baekjoon 10431

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+        int[][] arr = new int[t][2];
 
-        bw.write(n % 2 == 1 ? "SK" : "CY");
+        while(t-- > 0) {
+            int[] line = new int[20];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            for(int i = 0; i < 20; i++) line[i] = Integer.parseInt(st.nextToken());
+            int cnt = 0;
 
+            for(int i = 19; i > 0; i--) {
+                for(int j = i - 1; j >= 0; j--) {
+                    if(line[i] < line[j]) {
+                        cnt++;
+                    }
+                }
+            }
+
+            arr[n - 1][0] = n;
+            arr[n - 1][1] = cnt;
+        }
+
+        for(int[] i : arr) {
+            bw.write(i[0] + " " + i[1] + "\n");
+        }
         bw.flush();
         bw.close();
         br.close();
