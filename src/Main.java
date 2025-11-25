5b60c005fd2f834b64bf0f77b8ab56e6eae3943a
ksbuin <ksubin1017@gmail.com>
2025-11-25 10:17:38 +0900

    baekjoon 23971

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Main main = new Main();
-
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < n; i++) {
-            arr[i] += Integer.parseInt(st.nextToken());
-        }
+        int y = (h + n) / (n + 1);
+        int x = (w + m) / (m + 1);
+        int answer = x * y;
 
-        st = new StringTokenizer(br.readLine());
-
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-
-        long answer = 0;
-
-        for(int i = 0; i < n; i++) {
-            int students = arr[i];
-            students -= b;
-            answer++;
-
-            if(students > 0) {
-                answer += (students + c - 1) / c;
-            }
-        }
 
         bw.write(answer + "\n");
         bw.flush();
