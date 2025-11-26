9df0da7baec4bb1c49ed695e9e7b21e2b4a67d8b
ksbuin <ksubin1017@gmail.com>
2025-11-26 11:27:38 +0900

    baekjoon 25757

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        while(true) {
-            String s = br.readLine();
-            if(s.equals("end")) break;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            boolean hasVowel = false;
-            boolean rule2 = true;
-            boolean rule3 = true;
+        int n = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
 
-            int vowelCnt = 0;
-            int consonantCnt = 0;
+        Set<String> players = new HashSet<>();
 
-            for(int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-
-                boolean isVowel = isVowel(c);
-
-                if(isVowel) {
-                    hasVowel = true;
-                    vowelCnt++;
-                    consonantCnt = 0;
-                } else {
-                    consonantCnt++;
-                    vowelCnt = 0;
-                }
-
-                if(vowelCnt == 3 || consonantCnt == 3) rule2 = false;
-
-                if(i > 0) {
-                    char prev = s.charAt(i - 1);
-                    if(prev == c) {
-                        if(!(c == 'e' || c == 'o')) rule3 = false;
-                    }
-                }
-            }
-
-            if(hasVowel && rule2 && rule3) sb.append("<").append(s).append("> is acceptable.\n");
-            else sb.append("<").append(s).append("> is not acceptable.\n");
+        for(int i = 0; i < n; i++) {
+            players.add(br.readLine());
         }
 
-        bw.write(sb.toString());
+        int answer = 0;
 
+        if(game.equals("Y")) answer = players.size();
+        if(game.equals("F")) answer = players.size() / 2;
+        if(game.equals("O")) answer = players.size() / 3;
+
+        bw.write(answer + "\n");
         bw.flush();
         bw.close();
         br.close();
