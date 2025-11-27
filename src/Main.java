d01c8a71e3531c41aa5f36f3d3c58ecd1a04e0d8
ksbuin <ksubin1017@gmail.com>
2025-11-27 11:47:33 +0900

    beakjoon 20125

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,88 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static boolean[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        String game = st.nextToken();
-
-        Set<String> players = new HashSet<>();
-
-        for(int i = 0; i < n; i++) {
-            players.add(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int heartX = 0;
+        int heartY = 0;
+        graph = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            String[] sArr = br.readLine().split("");
+            for (int j = 1; j <= sArr.length; j++) {
+                if (sArr[j - 1].equals("*")) {
+                    graph[i][j] = true;
+                }
+            }
         }
+        outer:
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (graph[i + 1][j] && graph[i - 1][j] && graph[i][j + 1] && graph[i][j - 1]) {
+                    heartX = j;
+                    heartY = i;
+                    break outer;
+                }
+            }
+        }
+        bw.write((heartY + " " + heartX) + "\n");
 
-        int answer = 0;
+        // 왼쪽 팔
+        int idx = -1;
+        int cnt = 0;
+        while (heartX + idx <= n && graph[heartY][heartX + idx]) {
+            idx--;
+            cnt++;
+        }
+        sb.append(cnt + " ");
 
-        if(game.equals("Y")) answer = players.size();
-        if(game.equals("F")) answer = players.size() / 2;
-        if(game.equals("O")) answer = players.size() / 3;
+        // 오른쪽 팔
+        idx = 1;
+        cnt = 0;
+        while (heartX + idx <= n && graph[heartY][heartX + idx]) {
+            idx++;
+            cnt++;
+        }
+        sb.append(cnt + " ");
 
-        bw.write(answer + "\n");
+        // 허리
+        idx = 1;
+        cnt = 0;
+        int waistY = 0;
+        while (graph[heartY + idx][heartX]) {
+            idx++;
+            cnt++;
+        }
+        waistY = heartY + idx - 1;
+        sb.append(cnt + " ");
+
+        // 왼쪽 다리
+        idx = 1;
+        cnt = 0;
+        while(waistY + idx <= n && graph[waistY + idx][heartX - 1]) {
+            idx++;
+            cnt++;
+        }
+        sb.append(cnt + " ");
+
+        // 오른쪽 다리
+        idx = 1;
+        cnt = 0;
+        while(waistY + idx <= n && graph[waistY + idx][heartX + 1]) {
+            idx++;
+            cnt++;
+        }
+        sb.append(cnt + " ");
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
