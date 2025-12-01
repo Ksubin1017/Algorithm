b2c4d116ff151bd3dd573588d9ff03dcf3b3020c
ksbuin <ksubin1017@gmail.com>
2025-12-01 11:56:11 +0900

    baekjoon 1244

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,54 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] light = new int[n + 1];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int target = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
 
-        Integer[] arr = new Integer[n];
+        for (int i = 1; i <= n; i++) {
+            light[i] = Integer.parseInt(st.nextToken());
+        }
 
-        if (n > 0) {
+        int sCnt = Integer.parseInt(br.readLine());
+
+        int[][] sArr = new int[sCnt][2];
+
+        for(int i = 0; i < sCnt; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
+            sArr[i][0] = Integer.parseInt(st.nextToken());
+            sArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < sCnt; i++) {
+            if(sArr[i][0] == 1) {
+                int num = sArr[i][1];
+                for(int j = num; j <= n; j += num) {
+                    light[j] = 1 - light[j];
+                }
+            } else {
+                int num = sArr[i][1];
+                int lt = num - 1;
+                int rt = num + 1;
+
+                while (lt >= 1 && rt <= n && light[lt] == light[rt]) {
+                    lt--;
+                    rt++;
+                }
+
+                for(int j = lt + 1; j <= rt - 1; j++) {
+                    light[j] = 1 - light[j];
+                }
             }
         }
 
-        if (p == 0) {
-            bw.write("-1");
-            bw.flush();
-            return;
+        for(int i = 1; i <= n; i++) {
+            bw.write(light[i] + " ");
+            if(i % 20 == 0) bw.write("\n");
         }
 
-        if (n == p && n > 0) {
-            Arrays.sort(arr, Collections.reverseOrder());
-            if (target <= arr[arr.length - 1]) {
-                bw.write("-1");
-                bw.flush();
-                return;
-            }
-        }
-
-        int rank = 1;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > target) rank++;
-            else break;
-        }
-
-        bw.write(String.valueOf(rank));
         bw.flush();
+        bw.close();
     }
 }
