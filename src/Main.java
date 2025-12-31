123ab14c0ca6f4ce760d961aa4864c631eda52af
ksbuin <ksubin1017@gmail.com>
2025-12-31 11:18:42 +0900

    baekjoon 1747

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static boolean[] isPrime;
+
+    public static void isPrime(int n) {
+        isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+
+    public static boolean isPalindrome(String x) {
+        int left = 0, right = x.length() - 1;
+
+        while(left < right) {
+            if(x.charAt(left) != x.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            List<Integer> score = new ArrayList<>();
-            for (int i = 1; i <= n; i++) score.add(Integer.parseInt(st.nextToken()));
-
-            HashMap<Integer, Integer> teamCnt = new HashMap<>();
-            for (int x : score) teamCnt.put(x, teamCnt.getOrDefault(x, 0) + 1);
-
-            List<Integer> filterScore = new ArrayList<>();
-            for (int x : score) if (teamCnt.get(x) == 6) filterScore.add(x);
-
-            HashMap<Integer, Integer> top4Sum = new HashMap<>();
-            HashMap<Integer, Integer> cnt4 = new HashMap<>();
-            HashMap<Integer, Integer> fifth = new HashMap<>();
-
-            int rank = 1;
-            for (int team : filterScore) {
-                int c = cnt4.getOrDefault(team, 0);
-
-                if (c < 4) {
-                    top4Sum.put(team, top4Sum.getOrDefault(team, 0) + rank);
-                    cnt4.put(team, c + 1);
-                } else if (c == 4) {
-                    fifth.put(team, rank);
-                    cnt4.put(team, 5);
-                }
-                rank++;
+        int target = Integer.parseInt(br.readLine());
+        isPrime(10000000);
+        while(true) {
+            if(isPrime[target] && isPalindrome(String.valueOf(target))) {
+                break;
+            }else {
+                target++;
             }
-
-            int winner = 0;
-            int minScore = Integer.MAX_VALUE;
-
-            for (int team : top4Sum.keySet()) {
-                int sum = top4Sum.get(team);
-
-                if (sum < minScore) {
-                    minScore = sum;
-                    winner = team;
-                } else if (sum == minScore) {
-                    if (fifth.get(team) < fifth.get(winner)) winner = team;
-                }
-            }
-
-            bw.write(winner + "\n");
         }
 
+        bw.write(String.valueOf(target));
         bw.flush();
         bw.close();
     }
