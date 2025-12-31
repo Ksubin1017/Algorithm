816cdb2703e51d763089a7d02264548391e187d5
ksbuin <ksubin1017@gmail.com>
2025-12-31 11:56:24 +0900

    baekjoon 1652

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean[] isPrime;
-
-    public static void isPrime(int n) {
-        isPrime = new boolean[n+1];
-        Arrays.fill(isPrime, true);
-
-        isPrime[0] = false;
-        isPrime[1] = false;
-
-        for(int i = 2; i <= Math.sqrt(n); i++) {
-            if(isPrime[i]) {
-                for(int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-    }
-
-    public static boolean isPalindrome(String x) {
-        int left = 0, right = x.length() - 1;
-
-        while(left < right) {
-            if(x.charAt(left) != x.charAt(right)) return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int target = Integer.parseInt(br.readLine());
-        isPrime(10000000);
-        while(true) {
-            if(isPrime[target] && isPalindrome(String.valueOf(target))) {
-                break;
-            }else {
-                target++;
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] graph = new boolean[110][110];
+
+        for(int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < n; j++) {
+                if(str.charAt(j) == '.') {
+                    graph[i][j] = true;
+                } else {
+                    graph[i][j] = false;
+                }
             }
         }
 
-        bw.write(String.valueOf(target));
+        int rcnt = 0;
+        int lcnt = 0;
+        int row = 0;
+        int col = 0;
+
+        for(int i = 0; i < n; i++) {
+            rcnt = 0;
+            lcnt = 0;
+            for(int j = 0; j < n; j++) {
+                if(graph[i][j]) rcnt++;
+                else {
+                    if(rcnt >= 2) row++;
+                    rcnt = 0;
+                }
+
+                if(graph[j][i]) lcnt++;
+                else {
+                    if(lcnt >= 2) col++;
+                    lcnt = 0;
+                }
+            }
+            if(rcnt >= 2) row++;
+            if(lcnt >= 2) col++;
+        }
+
+
+        bw.write(row + " " + col);
         bw.flush();
         bw.close();
     }
