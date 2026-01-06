3341d56255e4d5119324e81b5c6fb5d5de2d3519
ksbuin <ksubin1017@gmail.com>
2026-01-06 12:10:33 +0900

    baekjoon 16953

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, cnt1, cnt2;
-    static char[][] graph;
-    static boolean[][] visited;
+    static int a, b;
+    static long bfs() {
+        Queue<long[]> q = new ArrayDeque<>();
+        q.offer(new long[]{a, 1});
 
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+        while(!q.isEmpty()) {
+            long[] cur = q.poll();
+            long pos = cur[0];
+            long cnt = cur[1];
 
-    static void dfs(int y, int x) {
-        visited[y][x] = true;
+            if(pos == b) return cnt;
+            if(pos > b) continue;
 
-        for(int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-
-            if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-
-            if(!visited[ny][nx] && graph[ny][nx] == graph[y][x]) dfs(ny, nx);
+            q.offer(new long[]{pos * 2, cnt + 1});
+            q.offer(new long[]{pos * 10 + 1, cnt + 1});
         }
 
+        return -1;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
-        graph = new char[n][n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            graph[i] = br.readLine().toCharArray();
-        }
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[n][n];
+        bw.write(String.valueOf(bfs()));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    dfs(i, j);
-                    cnt1++;
-                }
-            }
-        }
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(graph[i][j] == 'G') graph[i][j] = 'R';
-            }
-        }
-
-        visited = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    dfs(i, j);
-                    cnt2++;
-                }
-            }
-        }
-
-        System.out.println(cnt1 + " " + cnt2);
+        bw.flush();
+        bw.close();
     }
 }
