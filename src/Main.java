8a5c543728ecc2b1ccb311856d5050a33f601dc7
ksbuin <ksubin1017@gmail.com>
2026-01-08 11:50:34 +0900

    baekjoon 2178

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int a, b;
-    static long bfs() {
-        Queue<long[]> q = new ArrayDeque<>();
-        q.offer(new long[]{a, 1});
+    static int x, y;
+    static boolean[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static int bfs() {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0, 1});
+        visited[0][0] = true;
 
         while(!q.isEmpty()) {
-            long[] cur = q.poll();
-            long pos = cur[0];
-            long cnt = cur[1];
+            int[] cur = q.poll();
+            int by = cur[0];
+            int bx = cur[1];
+            int cnt = cur[2];
 
-            if(pos == b) return cnt;
-            if(pos > b) continue;
+            if(by == y - 1 && bx == x - 1) return cnt;
 
-            q.offer(new long[]{pos * 2, cnt + 1});
-            q.offer(new long[]{pos * 10 + 1, cnt + 1});
+            for(int k = 0; k < 4; k++) {
+                int ny = by + dy[k];
+                int nx = bx + dx[k];
+
+                if(ny < 0 || ny >= y || nx < 0 || nx >= x) continue;
+                if(!graph[ny][nx]) continue;
+                if(visited[ny][nx]) continue;
+
+                visited[ny][nx] = true;
+
+                q.offer(new int[]{ny, nx, cnt + 1});
+            }
+
         }
 
         return -1;
@@ -28,8 +45,18 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+
+        graph = new boolean[y][x];
+        visited = new boolean[y][x];
+
+        for(int i = 0; i < y; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < x; j++) {
+                graph[i][j] = line.charAt(j) == '1';
+            }
+        }
 
         bw.write(String.valueOf(bfs()));
 
