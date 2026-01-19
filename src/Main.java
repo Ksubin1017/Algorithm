2be2c2af2f0571a3520f44d8d23fe8d434f6f038
ksbuin <ksubin1017@gmail.com>
2026-01-19 11:50:58 +0900

    baekjoon 1926

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static boolean[][] graph;
+    static boolean[][] visited;
+    static int n, m, cnt, area, maxWidth;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static void dfs(int y, int x) {
+        visited[y][x] = true;
+        area++;
+
+        for(int k = 0; k < 4; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+            if(!visited[ny][nx] && graph[ny][nx]) {
+                dfs(ny, nx);
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[9];
-        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-        }
+        graph = new boolean[510][510];
+        visited = new boolean[510][510];
 
-        for(int i = 0; i < 8; i++) {
-            for(int j = i + 1; j < 9; j++) {
-                if(sum - arr[i] - arr[j] == 100) {
-                    arr[i] = 0;
-                    arr[j] = 0;
-                    Arrays.sort(arr);
-                    for(int k = 2; k < arr.length; k++) {
-                        System.out.println(arr[k]);
-                    }
-                    return;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                if(Integer.parseInt(st.nextToken()) == 1) {
+                    graph[i][j] = true;
                 }
             }
         }
 
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(!visited[i][j] && graph[i][j]) {
+                    area = 0;
+                    dfs(i, j);
+                    cnt++;
+                    maxWidth = Math.max(maxWidth, area);
+                }
+            }
+        }
+
+        bw.write(cnt + "\n" + maxWidth);
         bw.flush();
         bw.close();
-
     }
 }
