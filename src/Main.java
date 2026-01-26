31e3b1fac39df6a4ab4b63c37876e561b05b197e
ksbuin <ksubin1017@gmail.com>
2026-01-26 11:29:03 +0900

    baekjoon 2230

--- src/Main.java
+++ src/Main.java
@@ -2,83 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int N, M;
-    static List<Integer>[] partyOfPerson;
-    static List<Integer>[] peopleInParty;
-    static boolean[] knowTruth;
-    static boolean[] visitedParty;
-
-    static void dfs(int person) {
-        for (int party : partyOfPerson[person]) {
-            if (visitedParty[party]) continue;
-
-            visitedParty[party] = true;
-
-            for (int nextPerson : peopleInParty[party]) {
-                if (!knowTruth[nextPerson]) {
-                    knowTruth[nextPerson] = true;
-                    dfs(nextPerson);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
 
-        knowTruth = new boolean[N + 1];
-        partyOfPerson = new ArrayList[N + 1];
-        peopleInParty = new ArrayList[M + 1];
-        visitedParty = new boolean[M + 1];
-
-        for (int i = 1; i <= N; i++) {
-            partyOfPerson[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= M; i++) {
-            peopleInParty[i] = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        st = new StringTokenizer(br.readLine());
-        int cnt = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < cnt; i++) {
-            int person = Integer.parseInt(st.nextToken());
-            knowTruth[person] = true;
-        }
+        Arrays.sort(arr);
 
-        for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
+        int lt = 0;
+        int rt = 0;
+        int min = Integer.MAX_VALUE;
 
-            for (int j = 0; j < num; j++) {
-                int person = Integer.parseInt(st.nextToken());
-                peopleInParty[i].add(person);
-                partyOfPerson[person].add(i);
+        while(lt <= rt && rt < arr.length) {
+            int minus = arr[rt] - arr[lt];
+            if(minus < m) rt++;
+            else if(minus > m) {
+                min = Math.min(min, minus);
+                lt++;
+            } else {
+                min = minus;
+                break;
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (knowTruth[i]) {
-                dfs(i);
-            }
-        }
-
-        int answer = 0;
-        for (int i = 1; i <= M; i++) {
-            boolean canLie = true;
-            for (int person : peopleInParty[i]) {
-                if (knowTruth[person]) {
-                    canLie = false;
-                    break;
-                }
-            }
-            if (canLie) answer++;
-        }
-
-        System.out.println(answer);
+        bw.write(min + "\n");
+        bw.flush();
+        bw.close();
     }
 }
