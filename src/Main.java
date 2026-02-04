86236bac8179e084f2ea127cccef8ec249548afd
ksbuin <ksubin1017@gmail.com>
2026-02-04 10:59:39 +0900

    baekjoon 1940

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,35 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n  = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
 
-        st= new StringTokenizer(br.readLine());
-        int[] arr1 = new int[n];
+        int[] arr = new int[n];
+
         for(int i = 0; i < n; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        st= new StringTokenizer(br.readLine());
-        int[] arr2 = new int[m];
-        for(int i = 0; i < m; i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        int l = 0;
+        int r = n - 1;
+        int answer = 0;
+
+        while(l < r) {
+            if(arr[l] + arr[r] == m) {
+                answer++;
+                l++;
+                r--;
+            } else if(arr[l] + arr[r] < m){
+                l++;
+            } else {
+                r--;
+            }
         }
 
-        int i = 0;
-        int j = 0;
-
-        List<Integer> answer = new ArrayList<>();
-
-        while(i < n && j < m) {
-            if(arr1[i] <= arr2[j]) answer.add(arr1[i++]);
-            else answer.add(arr2[j++]);
-        }
-
-        while(i < n) answer.add(arr1[i++]);
-        while(j < m) answer.add(arr2[j++]);
-
-        for(int x : answer) {
-            bw.write(x + " ");
-        }
+        bw.write(answer + "\n");
         bw.flush();
         bw.close();
     }
