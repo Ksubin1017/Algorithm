43caf0f6ad6098c68435cae2de3ce7dc6a5aae71
ksbuin <ksubin1017@gmail.com>
2026-02-05 11:14:19 +0900

    baekjoon 1337

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
-            if(n == 0 && m == 0) break;
+        int[] arr = new int[n];
 
-            int[] a = new int[n];
-            int[] b = new int[m];
-
-            for(int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(br.readLine());
-            }
-
-            for(int i = 0; i < m; i++) {
-                b[i] = Integer.parseInt(br.readLine());
-            }
-
-            int i = 0;
-            int j = 0;
-            int answer = 0;
-
-            while(i < n && j < m) {
-                if(a[i] == b[j]) {
-                    answer++;
-                    i++;
-                    j++;
-                } else if(a[i] < b[j]) {
-                    i++;
-                } else {
-                    j++;
-                }
-            }
-
-            bw.write(answer + "\n");
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
+        Arrays.sort(arr);
+
+        int answer = 5;
+        int l = 0;
+
+        for(int r = 0; r < n; r++) {
+            while(arr[r] - arr[l] > 4) {
+                l++;
+            }
+            int len = r - l + 1;
+            answer = Math.min(answer, 5 - len);
+        }
+
+        bw.write(answer + "\n");
         bw.flush();
         bw.close();
     }
