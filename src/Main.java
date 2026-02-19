dd753565d14c1709fd9163f8750e9489df8f7652
ksbuin <ksubin1017@gmail.com>
2026-02-19 11:27:31 +0900

    baekjoon 30088

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[n];
 
-        int[][] arr = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int count = Integer.parseInt(st.nextToken());
+            long sum = 0;
 
-        for(int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < count; j++) {
+                sum += Long.parseLong(st.nextToken());
             }
+            arr[i] = sum;
         }
 
-        int k = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
 
-        for(int a = 0; a < k; a++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+        long total = 0;
+        long answer = 0;
 
-            int sum = 0;
-            for(int p = i; p <= x; p++) {
-                for(int q = j; q <= y; q++) {
-                    sum += arr[p][q];
-                }
-            }
-
-            bw.write(sum + "\n");
+        for (long s : arr) {
+            total += s;
+            answer += total;
         }
 
+
+        bw.write(answer + "\n");
         bw.flush();
         bw.close();
     }
