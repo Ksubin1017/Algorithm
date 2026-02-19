dcec8986d4ab46cd4d53a3fa96a327d2afa1763f
ksbuin <ksubin1017@gmail.com>
2026-02-19 11:06:23 +0900

    baekjoon 2167

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int d(int n) {
-        int sum = n;
-
-        while(n > 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-
-        return sum;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] check = new boolean[10001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i <= 10000; i++) {
-            int dn = d(i);
-            if(dn <= 10000) check[dn] = true;
-        }
+        int[][] arr = new int[n + 1][m + 1];
 
-        for(int i = 1; i <= 10000; i++) {
-            if(!check[i]) {
-                bw.write(i + "\n");
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        int k = Integer.parseInt(br.readLine());
+
+        for(int a = 0; a < k; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+            for(int p = i; p <= x; p++) {
+                for(int q = j; q <= y; q++) {
+                    sum += arr[p][q];
+                }
+            }
+
+            bw.write(sum + "\n");
+        }
+
         bw.flush();
         bw.close();
     }
