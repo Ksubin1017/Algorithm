b39834cc01682f47def2d655d16f72020f2641cb
ksbuin <ksubin1017@gmail.com>
2026-02-23 11:21:38 +0900

    baekjoon 9094

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,27 @@ import java.math.BigInteger;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BigInteger p = new BigInteger(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
 
-        for (int i = 2; i < k; i++) {
-            if (p.mod(BigInteger.valueOf(i)).equals(BigInteger.ZERO)) {
-                System.out.println("BAD " + i);
-                return;
+        while(t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int answer = 0;
+
+            for(int a = 1; a < n - 1; a++) {
+                for(int b = a + 1; b < n; b++) {
+                    if(((a * a) + (b * b) + m) % (a * b) == 0) {
+                        answer++;
+                    }
+                }
             }
+
+            System.out.println(answer);
+
         }
 
-        System.out.println("GOOD");
+
     }
 }
