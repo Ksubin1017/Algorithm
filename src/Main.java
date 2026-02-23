f8263489dc4856e443661f58309622df9762fc0e
ksbuin <ksubin1017@gmail.com>
2026-02-23 11:01:31 +0900

    baekjoon 17614

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        long[] arr = new long[n];
+        int answer = 0;
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int count = Integer.parseInt(st.nextToken());
-            long sum = 0;
-
-            for (int j = 0; j < count; j++) {
-                sum += Long.parseLong(st.nextToken());
+        for(int i = 1; i <= n; i++) {
+            String s = String.valueOf(i);
+            for(char c : s.toCharArray()) {
+                if(c == '3' || c == '6' || c == '9') answer++;
             }
-            arr[i] = sum;
         }
 
-        Arrays.sort(arr);
-
-        long total = 0;
-        long answer = 0;
-
-        for (long s : arr) {
-            total += s;
-            answer += total;
-        }
-
-
-        bw.write(answer + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
     }
 }
