73d9da03b81105cade4ecc382771f73b0f45883f
ksbuin <ksubin1017@gmail.com>
2026-02-26 11:03:33 +0900

    baekjoon 10448

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        List<String> list = new ArrayList<>();
+        int t = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int answer = 0;
 
-        for(int i = 0; i < n; i++) {
-            list.add(st.nextToken());
+            for (int i = 1; i <= 45; i++) {
+                for (int j = 1; j <= 45; j++) {
+                    for (int k = 1; k <= 45; k++) {
+                        int t1 = (i * (i + 1) / 2);
+                        int t2 = (j * (j + 1) / 2);
+                        int t3 = (k * (k + 1) / 2);
+
+                        if (t1 + t2 + t3 == n) {
+                            answer = 1;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(answer);
         }
-
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < n - 1; i++) {
-            list.remove(st.nextToken());
-        }
-
-        for(String s : list) System.out.println(list.get(0));
     }
 }
