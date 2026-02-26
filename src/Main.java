10f10c06351f16ce07b0748b9ac08810a7e601b4
ksbuin <ksubin1017@gmail.com>
2026-02-26 11:09:15 +0900

    baekjoon 11170

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,19 @@ public class Main {
 
         int t = Integer.parseInt(br.readLine());
 
+
         while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            int answer = 0;
-
-            for (int i = 1; i <= 45; i++) {
-                for (int j = 1; j <= 45; j++) {
-                    for (int k = 1; k <= 45; k++) {
-                        int t1 = (i * (i + 1) / 2);
-                        int t2 = (j * (j + 1) / 2);
-                        int t3 = (k * (k + 1) / 2);
-
-                        if (t1 + t2 + t3 == n) {
-                            answer = 1;
-                        }
-                    }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int cnt = 0;
+            for(int i = n; i <= m; i++) {
+                for(char c : String.valueOf(i).toCharArray()) {
+                    if(c == '0') cnt++;
                 }
             }
 
-            System.out.println(answer);
+            System.out.println(cnt);
         }
     }
 }
