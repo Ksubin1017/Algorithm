80f406ea517096e236c167357394b7dee8e6eda0
ksbuin <ksubin1017@gmail.com>
2026-02-27 11:57:56 +0900

    baekjoon 5555

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String target = br.readLine();
         int n = Integer.parseInt(br.readLine());
+        int answer = 0;
 
-        int max = Integer.MIN_VALUE;
-        int winner = 0;
+        for(int i = 1; i <= n; i++) {
+            String s = br.readLine().repeat(2);
 
-        for(int person = 1; person <= n; person++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int[] card = new int[5];
-
-            for(int i = 0; i < 5; i++) {
-                card[i] = Integer.parseInt(st.nextToken());
-            }
-
-            int top = 0;
-
-            for(int i = 0; i < 3; i++) {
-                for(int j = i + 1; j < 4; j++) {
-                    for(int k = j + 1; k < 5; k++) {
-                        int sum = card[i] + card[j] + card[k];
-                        top = Math.max(top, sum % 10);
-                    }
-                }
-            }
-
-            if(top >= max) {
-                winner = person;
-                max = top;
-            }
+            if(s.contains(target)) answer++;
         }
 
-        System.out.println(winner);
+        System.out.println(answer);
     }
 }
