5533ea4f67bd5976f259fedd763f571f837a04e5
ksbuin <ksubin1017@gmail.com>
2026-03-09 11:39:21 +0900

    baekjoon 5671

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,39 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (true) {
 
-        int n = Integer.parseInt(st.nextToken());
-        String d = st.nextToken();
-        int answer = 0;
+            String line = br.readLine();
+            if(line == null) break;
 
-        for(int i = 1; i <= n; i++) {
-            String target = String.valueOf(i);
+            StringTokenizer st = new StringTokenizer(line);
 
-            for(String c : target.split("")) {
-                if(c.equals(d)) answer++;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int cnt = 0;
+
+            for (int i = a; i <= b; i++) {
+                char[] arr = String.valueOf(i).toCharArray();
+                boolean flag = true;
+
+                for(int x = 0; x < arr.length; x++) {
+                    for(int y = x + 1; y < arr.length; y++) {
+                        if(arr[x] == arr[y]) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+
+                if(flag) cnt++;
             }
-        }
 
-        System.out.println(answer);
+            System.out.println(cnt);
+        }
     }
 }
