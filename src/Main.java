07840e6ce287f812aa9b89453eba1d5ac5c0be88
ksbuin <ksubin1017@gmail.com>
2026-03-10 11:49:13 +0900

    baekjoon 17478

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,35 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine();
-        int[] cnt = new int[10];
+        int n = Integer.parseInt(br.readLine());
 
-        for(char c : s.toCharArray()) {
-            cnt[c - '0']++;
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n" +
+                "\"재귀함수가 뭔가요?\"\n" +
+                "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" +
+                "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" +
+                "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        dfs(n, 1);
+    }
+
+    static void dfs(int num, int cnt) {
+        String str = "____".repeat(cnt) + "\"재귀함수가 뭔가요?\"\n"
+                + "____".repeat(cnt) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n"
+                + "____".repeat(cnt) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n"
+                + "____".repeat(cnt) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
+
+        if(cnt < num) System.out.println(str);
+
+        if(cnt >= num) {
+            str = "____".repeat(cnt) + "\"재귀함수가 뭔가요?\"\n"
+                    + "____".repeat(cnt) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+
+            while(cnt >= 0) {
+                str += "____".repeat(cnt) + "라고 답변하였지. \n";
+                cnt--;
+            }
+            System.out.println(str);
+            return;
         }
-
-        int max = (cnt[6] + cnt[9] + 1) / 2;
-
-        for(int i = 0; i < 10; i++) {
-            if(i == 6 || i == 9) continue;
-            max= Math.max(max, cnt[i]);
-        }
-
-        System.out.println(max);
+        dfs(num, cnt + 1);
     }
 }
