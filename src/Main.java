0e72267dd6c9353f98e54df989071f7524d63d3b
ksbuin <ksubin1017@gmail.com>
2026-03-10 11:29:46 +0900

    baekjoon 1475

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean isPalindrome(String s) {
-        int l = 0;
-        int r = s.length() - 1;
-
-        while (l < r) {
-            if(s.charAt(l) != s.charAt(r)) return false;
-            l++;
-            r--;
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int[] cnt = new int[10];
 
-        while(t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            String[] arr = new String[n];
-
-            boolean flag = false;
-
-            for(int i = 0; i < n; i++) {
-                arr[i] = br.readLine();
-            }
-
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(i == j) continue;
-                    flag = isPalindrome(arr[i] + arr[j]);
-                    if(flag) {
-                        System.out.println(arr[i] + arr[j]);
-                        break;
-                    }
-                }
-
-                if(flag) break;
-            }
-
-            if(!flag) {
-                System.out.println(0);
-            }
+        for(char c : s.toCharArray()) {
+            cnt[c - '0']++;
         }
+
+        int max = (cnt[6] + cnt[9] + 1) / 2;
+
+        for(int i = 0; i < 10; i++) {
+            if(i == 6 || i == 9) continue;
+            max= Math.max(max, cnt[i]);
+        }
+
+        System.out.println(max);
     }
 }
