349efaa5ce0fe9953cab7d5e41c4809cd44f4660
ksbuin <ksubin1017@gmail.com>
2026-03-12 15:23:46 +0900

    baekjoon 2161

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int n  = Integer.parseInt(br.readLine());
 
-        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n" +
-                "\"재귀함수가 뭔가요?\"\n" +
-                "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" +
-                "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" +
-                "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-        dfs(n, 1);
-    }
+        Queue<Integer> q = new ArrayDeque<>();
 
-    static void dfs(int num, int cnt) {
-        String str = "____".repeat(cnt) + "\"재귀함수가 뭔가요?\"\n"
-                + "____".repeat(cnt) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n"
-                + "____".repeat(cnt) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n"
-                + "____".repeat(cnt) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
-
-        if(cnt < num) System.out.println(str);
-
-        if(cnt >= num) {
-            str = "____".repeat(cnt) + "\"재귀함수가 뭔가요?\"\n"
-                    + "____".repeat(cnt) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
-
-            while(cnt >= 0) {
-                str += "____".repeat(cnt) + "라고 답변하였지. \n";
-                cnt--;
-            }
-            System.out.println(str);
-            return;
+        for(int i = 1; i <= n; i++) {
+            q.offer(i);
         }
-        dfs(num, cnt + 1);
+
+        while(q.size() > 1) {
+            System.out.print(q.poll() + " ");
+            q.add(q.poll());
+        }
+
+        System.out.print(q.poll());
     }
 }
