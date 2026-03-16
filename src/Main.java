4ef76cc93bf49bf41331d3e847e0e810d9328801
ksbuin <ksubin1017@gmail.com>
2026-03-16 20:32:10 +0900

    baekjoon 31575

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N, M;
+    static String answer = "No";
+    static boolean[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {0, 1};
+    static int[] dy = {1, 0};
+
+    static void dfs(int y, int x) {
+        visited[y][x] = true;
+
+        if(y == M - 1 && x == N - 1) {
+            answer = "Yes";
+            return;
+        }
+
+        for(int k = 0; k < 2; k++) {
+            int ny = y + dy[k];
+            int nx = x + dx[k];
+
+            if(ny < 0 || ny >= M || nx < 0 || nx >= N) continue;
+
+            if(graph[ny][nx] && !visited[ny][nx]) {
+                visited[ny][nx] = true;
+                dfs(ny,nx);
+            }
+        }
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        graph = new boolean[M][N];
+        visited = new boolean[M][N];
 
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                int x = Integer.parseInt(st.nextToken());
+
+                if(x == 1) graph[i][j] = true;
+            }
         }
 
-        boolean[] visited = new boolean[n];
+        dfs(0, 0);
+        System.out.println(answer);
 
-        int cur = 0;
-        int cnt = 0;
-
-        while(true) {
-            if(cur == k) {
-                System.out.println(cnt);
-                return;
-            }
-
-            if(visited[cur]) {
-                System.out.println(-1);
-                return;
-            }
-
-            visited[cur] = true;
-            cur = arr[cur];
-            cnt++;
-        }
     }
 }
