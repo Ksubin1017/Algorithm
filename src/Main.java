4fb4f3cad2e2024cacafdecb215abf37753b80cc
ksbuin <ksubin1017@gmail.com>
2026-03-18 11:19:41 +0900

    baekjoon 1743

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
+    static int N, M, K;
+    static boolean[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
 
-    static int bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        visited = new boolean[N + 1];
-
-        q.add(start);
-        visited[start] = true;
-
+    static int dfs(int y, int x) {
+        visited[y][x] = true;
         int cnt = 1;
 
-        while(!q.isEmpty()) {
-            int cur = q.poll();
+        for (int k = 0; k < 4; k++) {
+            int ny = y + dy[k];
+            int nx = x + dx[k];
 
-            for(int next : graph[cur]) {
-                if(!visited[next]) {
-                    visited[next] = true;
-                    q.add(next);
-                    cnt++;
-                }
+            if (ny < 0 || ny > N || nx < 0 || nx > M) continue;
+
+            if (graph[ny][nx] && !visited[ny][nx]) {
+                cnt += dfs(ny, nx);
             }
         }
 
@@ -33,35 +29,34 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        graph = new boolean[N + 1][M + 1];
+        visited = new boolean[N + 1][M + 1];
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 1; i <= K; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            graph[b].add(a);
+            graph[a][b] = true;
+
         }
 
-        int[] result = new int[N + 1];
         int max = 0;
 
-        for(int i = 1; i <= N; i++) {
-            result[i] = bfs(i);
-            max = Math.max(max, result[i]);
-        }
-
-        for(int i = 1; i <= N; i++) {
-            if(result[i] == max) {
-                System.out.print(i + " ");
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (graph[i][j] && !visited[i][j]) {
+                    int size = dfs(i, j);
+                    max = Math.max(max, size);
+                }
             }
         }
+
+        System.out.println(max);
     }
 }
