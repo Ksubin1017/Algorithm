e34f04f59c3b801f223f7d056451f5e305f4e293
ksbuin <ksubin1017@gmail.com>
2026-03-23 11:39:48 +0900

    baekjoon 11060

--- src/Main.java
+++ src/Main.java
@@ -2,59 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static char[][] board;
-    static boolean[][] visited;
-    static int N, M, curN, curM, CNT = 0;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int[] arr;
+    static boolean[] visited;
     static int bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{curN, curM});
+        q.offer(new int[]{0, 0});
 
         while(!q.isEmpty()) {
             int[] cur = q.poll();
-            int posN = cur[0];
-            int posM = cur[1];
+            int idx = cur[0];
+            int cnt = cur[1];
 
-            if(board[posN][posM] == 'P') CNT++;
-
-            for(int i = 0; i < dx.length; i++) {
-                int ny = posN + dy[i];
-                int nx = posM + dx[i];
-
-                if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-
-                if(!visited[ny][nx] && board[ny][nx] != 'X') {
-                    visited[ny][nx] = true;
-                    q.offer(new int[]{ny, nx});
+            if(idx == arr.length - 1) return cnt;
+            for(int i = 1; i <= arr[idx]; i++) {
+                if(arr[idx] == 0 || idx + i >= arr.length) continue;
+                if(!visited[idx + i]) {
+                    visited[idx + i] = true;
+                    q.offer(new int[]{idx + i, cnt + 1});
                 }
             }
         }
 
-        return CNT;
+        return -1;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        visited = new boolean[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        board = new char[N + 1][M + 1];
-        visited = new boolean[N + 1][M + 1];
-
-        for(int i = 0; i < N; i++) {
-            char[] arr = br.readLine().toCharArray();
-            for(int j = 0; j < M; j++) {
-                if(arr[j] == 'I') {
-                    curN = i;
-                    curM = j;
-                }
-                board[i][j] = arr[j];
-            }
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(bfs() != 0 ? bfs() : "TT");
+        System.out.println(bfs());
     }
 }
