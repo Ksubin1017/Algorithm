621d1688a0a883fdb4b4a3ac1b11a9d812e88ebd
ksbuin <ksubin1017@gmail.com>
2026-03-26 10:59:11 +0900

    baekjoon 14496

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, A, B;
-    static int[] graph;
+    static int A, B, N, M;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
 
     static int bfs() {
@@ -11,29 +11,18 @@ public class Main {
         q.offer(new int[]{A, 0});
         visited[A] = true;
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int[] cur = q.poll();
             int loc = cur[0];
             int cnt = cur[1];
 
-            if (loc == B) return cnt;
+            if(loc == B) return cnt;
 
-            int jump = graph[loc];
-
-            for(int k = 1; loc + jump * k <= N; k++) {
-                int next = loc + jump * k;
-                if(visited[next]) continue;
-
-                visited[next] = true;
-                q.offer(new int[]{next, cnt + 1});
-            }
-
-            for(int k = 1; loc - jump * k >= 1; k++) {
-                int next = loc - jump * k;
-                if(visited[next]) continue;
-
-                visited[next] = true;
-                q.offer(new int[]{next, cnt + 1});
+            for(int next : graph[loc]) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    q.offer(new int[]{next, cnt + 1});
+                }
             }
         }
 
@@ -43,20 +32,29 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-
-        graph = new int[N + 1];
-        visited = new boolean[N + 1];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <= N; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
-        }
-
-        st = new StringTokenizer(br.readLine());
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x].add(y);
+            graph[y].add(x);
+        }
 
         System.out.println(bfs());
     }
