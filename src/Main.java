e6f713357d3290e7bc3f9c1f8ffd640af80ff5e8
ksbuin <ksubin1017@gmail.com>
2026-05-18 10:52:58 +0900

    프로그래머스 올바른 괄호

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Solution solution = new Solution();
-        int[] coins = {9, 4, 1};
-        System.out.println(solution.solution(coins, 12));
     }
 }
 
 class Solution {
-    public int solution(int[] coins, int amount) {
-        Queue<int[]> q = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        q.add(new int[]{amount, 0});
-        visited.add(amount);
+    boolean solution(String s) {
+        boolean answer = false;
+        Deque<Character> stack = new ArrayDeque<>();
+        char[] arr = s.toCharArray();
 
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curAmount = cur[0];
-            int cnt = cur[1];
-
-            if(curAmount == 0) return cnt;
-
-            for(int coin : coins) {
-                int nextAmount = curAmount - coin;
-                if(nextAmount < 0) continue;
-                if(!visited.contains(nextAmount)) {
-                    visited.add(nextAmount);
-                    q.add(new int[]{nextAmount, cnt + 1});
+        for(char c : arr) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
                 }
             }
         }
 
-        return -1;
+        if(stack.isEmpty()) {
+            answer = true;
+        }
+
+
+        return answer;
     }
 }
 
