6f1b52e3af691f9ddc274f792fde877e7c38e4dd
ksbuin <ksubin1017@gmail.com>
2026-05-18 11:19:54 +0900

    프로그래머스 프로세스

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,38 @@ public class Main {
 }
 
 class Solution {
-    boolean solution(String s) {
-        boolean answer = false;
-        Deque<Character> stack = new ArrayDeque<>();
-        char[] arr = s.toCharArray();
+    public int solution(int[] priorities, int location) {
+        Deque<int[]> q = new ArrayDeque<>();
 
-        for(char c : arr) {
-            if(c == '(') {
-                stack.push(c);
+        for(int i = 0; i < priorities.length; i++) {
+            q.offer(new int[]{priorities[i], i});
+        }
+
+        int order = 0;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            boolean higher = false;
+
+            for(int[] chk: q) {
+                if(cur[0] < chk[0]) {
+                    higher = true;
+                    break;
+                }
+            }
+
+            if(higher) {
+                q.offer(cur);
             } else {
-                if(!stack.isEmpty()) {
-                    stack.pop();
-                } else {
-                    return false;
+                order++;
+
+                if(cur[1] == location) {
+                    return order;
                 }
             }
         }
 
-        if(stack.isEmpty()) {
-            answer = true;
-        }
-
-
-        return answer;
+        return order;
     }
 }
 
