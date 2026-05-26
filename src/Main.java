bf49065db68ee5b17c3466e398a4b9b6de28f2bf
ksbuin <ksubin1017@gmail.com>
2026-05-26 10:55:05 +0900

    구름Level 피보나치 수

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 import java.io.*;
+
 class Main {
+    static final long MOD = 1000000007L;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[] coins = {40, 20, 10, 5, 1};
+        int k = Integer.parseInt(br.readLine());
 
-        int count = 0;
-
-        for(int coin : coins) {
-            count += n / coin;
-            n %= coin;
+        if(k == 1) {
+            System.out.println(0);
+            return;
         }
 
-        System.out.println(count);
+        long a = 0;
+        long b = 1;
+
+        for(int i = 3; i <= k; i++) {
+            long temp = (a + b) % MOD;
+            a = b;
+            b = temp;
+        }
+
+        System.out.println(b);
     }
 }
